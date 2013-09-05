38ad99b53ccff8ccb6c288b48f7a7902cade425a
Jonas M Luster <jml@jml.is>
2013-09-05 14:35:32 +0000

    js subdir, modernizr

--- functions.php
+++ functions.php
@@ -6,12 +6,13 @@ add_action( 'init', 'jml_menus' );
 add_theme_support( 'post-formats', array('aside','gallery','image','post','link') );
 add_editor_style( 'editor-style.css' );
 add_theme_support( 'automatic-feed-links' );
+add_action( 'admin_menu', 'textlove_add_menu' );
 
 function jml_scripts() {
   wp_enqueue_style( 'pure', $src = 'http://yui.yahooapis.com/pure/0.2.1/pure-min.css', $deps = false, $ver = '0.2.1', $media = 'all');
   wp_enqueue_style( 'style', $src = get_stylesheet_uri(), $deps = false, $ver = '1.0', $media = 'all' );
   wp_enqueue_style( 'font', $src = 'http://netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css', $deps = false, $ver = '3.2.1', $media = 'all' );
-
+  wp_enqueue_script('modernizr', $src = get_stylesheet_directory_uri()."/js/modernizr.js", $deps = false, $ver = "1.0", $in_footer = false);
   wp_enqueue_script( 'yui', $src = 'http://yui.yahooapis.com/3.10.1/build/yui/yui-min.js', $deps = false, $ver = '3.10.1', $in_footer = true );
 }
 
